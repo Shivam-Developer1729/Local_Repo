abe6e1ef658948d49770ab0e49f05b868801e0af
Shivam-Developer1729 <tandank9990@gmail.com>
2024-10-16 19:12:03 +0530

    Add initial Files

---
+++ code.java
@@ -0,0 +1,22 @@
+
+import java.util.Scanner;
+
+/**
+ * code
+ */
+public class code {
+
+    public static void main(String[] args) {
+
+        try (Scanner input = new Scanner(System.in)) {
+            System.out.print("Enter your Number: ");
+            int num = input.nextInt();
+            System.out.println("The table of " + num + " is: ");
+            for (int i = 1; i <= 10; i++) {
+                System.out.println(num + " X " + i + " = " + num*i );
+            }
+        }
+        
+        System.out.println("=== Code Execution Successful ===");
+    }
+}
